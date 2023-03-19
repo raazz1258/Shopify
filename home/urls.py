fcb08084093b9c5a15fa3d2aa29ce51b8a34c582
razz choudhary <maheshworchaudhary9@gmail.com>
2023-03-19 16:11:22 +0545

    item delete and edit feature added

--- home/urls.py
+++ home/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth import views as auth_views
 from .form import LoginForm
 
 urlpatterns = [
-    path('', views.index),
-    path('register/', views.register),
+    path('', views.index, name= 'index'),
+    path('register/', views.register, name= 'register'),
      path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login'),
 
 
--- item/urls.py
+++ item/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+app_name = 'item'
 
 urlpatterns = [
-    
-
-
-
+    path('', views.dashboard, name='dashboard'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
